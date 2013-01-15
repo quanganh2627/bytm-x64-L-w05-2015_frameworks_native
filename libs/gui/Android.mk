2a6310f8d43a1a2c4cb7172c21b3349bf5eda833
wanglili <lili.a.wang@intel.com>
2013-01-16 01:34:20 +0800

    [PORT FROM MAIN] Extend GFX maximum buffer number in BufferQueue from 32 to 64 to support VPP.[Video-MRFLD]
    BZ: 83081
    
    In current design, VPP(Video Post-Processing) will apply for
    extra graphic buffers from native window, the same buffer queue
    as decoder. Since Decoder will occupy 25 buffers as the peak
    value, GFX buffer limitation "32" cannot meet VPP's requirement.
    We'd like to extend it to 64 so far to enable VPP UC.
    
    DO NOT UPSTREAM
    
    Category: AOSP improvement
    Domain : Graphics-SF
    Origin: Internal
    Upstream-Candidate : No, proprietary
    
    Change-Id: Id1de9ff5c86a63bdc63b7686dc6521ac51a3b42e
    Signed-off-by: Tong, Bo <box.tong@intel.com>

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -54,6 +54,11 @@ ifeq ($(TARGET_BOARD_PLATFORM), clovertrail)
 	LOCAL_CFLAGS += -DUSE_WAIT_SYNC
 endif
 
+# for VPP support on MRFLD only
+ifeq ($(TARGET_HAS_VPP),true)
+    LOCAL_CFLAGS += -DGFX_BUF_EXT
+endif
+
 include $(BUILD_SHARED_LIBRARY)
 
 ifeq (,$(ONE_SHOT_MAKEFILE))
