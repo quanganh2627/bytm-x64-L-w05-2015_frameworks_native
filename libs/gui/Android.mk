7653623fd1c1f54637a935b5777fe1535a903214
Marc Mao <marc.mao@intel.com>
2013-02-27 19:13:17 +0800

    [PORT FROM MAIN]Enable framework native fence for PVR DDK1.10
    BZ: 102283
    
    Change-Id: I430b77caee0bc981a1387c13e157dd2a02608405
    Signed-off-by: Marc Mao <marc.mao@intel.com>
    Signed-off-by: yifeix.xue <yifeix.xue@intel.com>
    Reviewed-on: http://android.intel.com:8080/124032
    Reviewed-by: Liu, MinX Z <minx.z.liu@intel.com>
    Tested-by: Liu, MinX Z <minx.z.liu@intel.com>
    Reviewed-by: Xu, Randy <randy.xu@intel.com>
    Reviewed-by: Yao, Yong <yong.yao@intel.com>
    Reviewed-by: Zhang, Lei <lei.zhang@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- libs/gui/Android.mk
+++ libs/gui/Android.mk
@@ -54,6 +54,10 @@ ifeq ($(TARGET_BOARD_PLATFORM), tegra3)
 	LOCAL_CFLAGS += -DDONT_USE_FENCE_SYNC
 endif
 
+ifeq ($(TARGET_BOARD_PLATFORM), clovertrail)
+LOCAL_CFLAGS += -DUSE_NATIVE_FENCE_SYNC
+endif
+
 # for VPP support on MRFLD only
 ifeq ($(TARGET_HAS_VPP),true)
     LOCAL_CFLAGS += -DGFX_BUF_EXT
