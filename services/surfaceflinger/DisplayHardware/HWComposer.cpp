a831cd1d8544882fd17e35bc1bac7e18593d7d18
Marc Mao <marc.mao@intel.com>
2013-05-08 09:39:34 +0800

    Revert "Hack FBO flush issue in captureScreen"
    BZ: 102404
    
    Another fix 106542 will fix it.
    
    This reverts commit e944460f163f9cb706e6f8ea4c9817672a6ff53a.
    
    Change-Id: I550f48c44c88a543e67fc3c44cd950f0b48249d0
    Signed-off-by: Marc Mao <marc.mao@intel.com>
    Reviewed-on: http://android.intel.com:8080/106699
    Reviewed-by: Xu, Randy <randy.xu@intel.com>
    Tested-by: Xu, Randy <randy.xu@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- services/surfaceflinger/DisplayHardware/HWComposer.cpp
+++ services/surfaceflinger/DisplayHardware/HWComposer.cpp
@@ -738,14 +738,8 @@ int HWComposer::fbPost(int32_t id,
 }
 
 int HWComposer::fbCompositionComplete() {
-    if (mHwc && hwcHasApiVersion(mHwc, HWC_DEVICE_API_VERSION_1_1)) {
-       if (mHwc->reserved_proc[0]) {
-            int (*compositionComplete)(struct hwc_composer_device_1*, int) =
-                (int (*)(hwc_composer_device_1*, int))mHwc->reserved_proc[0];
-            compositionComplete(mHwc, 0);
-        }
+    if (mHwc && hwcHasApiVersion(mHwc, HWC_DEVICE_API_VERSION_1_1))
         return NO_ERROR;
-    }
 
     if (mFbDev->compositionComplete) {
         return mFbDev->compositionComplete(mFbDev);
