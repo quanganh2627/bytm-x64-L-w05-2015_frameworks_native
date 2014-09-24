698fa2b7a7100f224f77437b96d253d34fe38ad7
Junmin Zhu <junmin.zhu@intel.com>
2014-09-24 15:37:58 +0800

    Add hook patch for event processing before notify Motion event.
    Event processing library(named as touch filter) is used for reducing
    and purifying the touch event, it would help to decrease the
    power consumption. From the test on various devices and application,
    it achieves 0.3% ~ 20% around system power reduction
    when application running.
    
    This patch is used to invoke event processing library "libeventprocessing.so".
    When the library does exist under the "/system/lib",
    it would be invoked to pre-handle events before these events are notified.
    
    Change-Id: I68d5d7e8629dfd739f8e82b8a96fe234f042e0fe
    Tracked-On: https://jira01.devtools.intel.com/browse/IMINAN-3310
    Category: feature differentiation
    Domain: SENSORS-Hub
    Origin: internal
    Upstream-Candidate: no, proprietary
    Signed-off-by: Junmin Zhu <junmin.zhu@intel.com>
    Reviewed-on: https://android.intel.com:443/280331

--- services/inputflinger/InputListener.h
+++ services/inputflinger/InputListener.h
@@ -24,7 +24,19 @@
 namespace android {
 
 class InputListenerInterface;
-
+// --- Event Processing ---
+typedef int (*EventProcessingInit)();
+typedef int (*EventProcessingFini)();
+typedef bool (*EventProcessing)(nsecs_t eventTime, int32_t deviceId,
+                                uint32_t source, uint32_t policyFlags,
+                                int32_t action, int32_t flags,
+                                int32_t metaState, int32_t buttonState,
+                                int32_t edgeFlags, int32_t displayId,
+                                uint32_t pointerCount,
+                                PointerProperties* pointerProperties,
+                                PointerCoords* pointerCoords,
+                                float xPrecision, float yPrecision,
+                                nsecs_t downTime);
 
 /* Superclass of all input event argument objects */
 struct NotifyArgs {
@@ -187,6 +199,11 @@ public:
     void flush();
 
 private:
+    // Event processing.
+    void eventProcessingInitImpl();
+    void eventProcessingFiniImpl();
+    void* event_processing_handle;
+    EventProcessing event_processing;
     sp<InputListenerInterface> mInnerListener;
     Vector<NotifyArgs*> mArgsQueue;
 };
